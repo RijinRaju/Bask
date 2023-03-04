d656fc402afb395512469b0c06477b2d60a396bd
rijinraju <rijinraju856@gmail.com>
2023-03-04 20:09:32 +0530

    updated build1

--- src/Components/LoadingBar/LoadingBar.jsx
+++ src/Components/LoadingBar/LoadingBar.jsx
@@ -1,20 +1,19 @@
-import { CircularProgress } from '@material-ui/core'
-import React from 'react'
+import CircularProgress from "@mui/material/CircularProgress";
+import React from "react";
 
 function LoadingBar() {
   return (
-    <div style={{
-
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        height:'80vh',
-     
-    }}>
-        <CircularProgress/>
-
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
+    >
+      <CircularProgress />
     </div>
-  )
+  );
 }
 
-export default LoadingBar
+export default LoadingBar;
